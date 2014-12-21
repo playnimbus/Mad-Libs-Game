6039ab3f27252170b86fa13263b5959454776d78
juste119 <mike@playnimbus.com>
2014-12-21 15:40:49 -0500

    made changes to the painter javascript files so they compile to android

--- Assets/Scripts/Painter/GUIControls.js
+++ Assets/Scripts/Painter/GUIControls.js
@@ -15,20 +15,20 @@ static function RGBSlider (c : Color,label : String) {
 }
 
 static function RGBCircle (c : Color,label : String,colorCircle : Texture2D) {
-	r = GUILayoutUtility.GetAspectRect (1);
+	var r = GUILayoutUtility.GetAspectRect (1);
 	r.height = r.width -= 15;
-	r2 = Rect (r.x + r.width +5,r.y,10,r.height);
-	hsb = new HSBColor (c);//It is much easier to work with HSB colours in this case
+	var r2 = Rect (r.x + r.width +5,r.y,10,r.height);
+	var hsb = new HSBColor (c);//It is much easier to work with HSB colours in this case
 	
 	
-	cp = Vector2 (r.x+r.width/2,r.y+r.height/2);
+	var cp = Vector2 (r.x+r.width/2,r.y+r.height/2);
 	
 	if (Input.GetMouseButton (0)) {
 		var InputVector = Vector2.zero;
 		InputVector.x = cp.x - Event.current.mousePosition.x;
 		InputVector.y = cp.y - Event.current.mousePosition.y;
 		
-		hyp = Mathf.Sqrt( (InputVector.x * InputVector.x) + (InputVector.y * InputVector.y) );
+		var hyp = Mathf.Sqrt( (InputVector.x * InputVector.x) + (InputVector.y * InputVector.y) );
 		if (hyp <= r.width/2 + 5) {
 			hyp = Mathf.Clamp (hyp,0,r.width/2);
 			var a : float = Vector3.Angle(Vector3(-1,0,0), InputVector);
@@ -42,9 +42,9 @@ static function RGBCircle (c : Color,label : String,colorCircle : Texture2D) {
 		}
 	}
 	
-	hsb2 = new HSBColor (c);
+	var hsb2 = new HSBColor (c);
 	hsb2.b = 1;
-	c2 = hsb2.ToColor ();
+	var c2 = hsb2.ToColor ();
 	GUI.color = c2;
 	hsb.b = GUI.VerticalSlider (r2,hsb.b,1.0,0.0,"BWSlider","verticalsliderthumb");
 	
@@ -52,7 +52,7 @@ static function RGBCircle (c : Color,label : String,colorCircle : Texture2D) {
 	GUI.color.a = 1;
 	GUI.Box (r,colorCircle,GUIStyle.none);
 	
-	pos = (Vector2 (Mathf.Cos (hsb.h*360*Mathf.Deg2Rad),-Mathf.Sin (hsb.h*360*Mathf.Deg2Rad))*r.width*hsb.s/2);
+	var pos = (Vector2 (Mathf.Cos (hsb.h*360*Mathf.Deg2Rad),-Mathf.Sin (hsb.h*360*Mathf.Deg2Rad))*r.width*hsb.s/2);
 	
 	GUI.color = c;
 	GUI.Box (Rect(pos.x-5+cp.x,pos.y-5+cp.y,10,10),"","ColorcirclePicker");
