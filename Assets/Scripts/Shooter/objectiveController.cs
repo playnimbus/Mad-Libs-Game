5a4875a71bb086f2926846bac8cb33cb8bc6f032
Nick Mudry <nick@mudry.me>
2015-01-17 18:59:43 -0500

    Added Exit Door & Updated Objective

--- Assets/Scripts/Shooter/objectiveController.cs
+++ Assets/Scripts/Shooter/objectiveController.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class objectiveController : MonoBehaviour {
     public string currentObjective;
+
+    /* Key Objective Variables */
+    bool keyDropped;
+    bool keyPickedUp;
+
+    /* Other Variables */
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +17,69 @@ public class objectiveController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
+        //Running the update functoins for the current objectives. 
+	    switch (currentObjective)
+        {
+            case "FindKey" :
+                {
+                    objectiveKeyUpdate();
+                    break;
+                }
+            case "Survival" :
+                {
+                    break;
+                }
+        }
 	}
 
     public void SwitchObjective (string objective)
     {
+        //Setting the previous objective and calling the end functions for all previous objectives. 
+        string previousObjective;
+        previousObjective = currentObjective;
+
+        switch (previousObjective)
+        {
+            case "FindKey" :
+                {
+                    objectiveKeyEnd();
+                    break;
+                }
+            case "Survival" :
+                {
+                    break;
+                }
+        }
+        
+        //Switching over to the new objectives and calling their start functions. 
         currentObjective = objective;
+        switch (currentObjective)
+        {
+            case "FindKey":
+                {
+                    objectiveKeyStart();
+                    break;
+                }
+            case "Survival":
+                {
+                    break;
+                }
+        }
+    }
+
+    /*Key Objective */
+    void objectiveKeyStart()
+    {
+
+    }
+    void objectiveKeyUpdate()
+    {
+        
+    }
+    void objectiveKeyEnd()
+    {
+
     }
 
 }
