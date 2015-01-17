1918d94310711f2e70f09619b9a4fd39256d956c
Nick Mudry <nick@mudry.me>
2015-01-17 18:41:18 -0500

    Started Objective COntroller Scipt

---
+++ Assets/Scripts/Shooter/objectiveController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectiveController : MonoBehaviour {
+    public string currentObjective;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SwitchObjective (string objective)
+    {
+        currentObjective = objective;
+    }
+}
